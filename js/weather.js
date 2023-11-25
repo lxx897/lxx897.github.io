c26ea2cad1d5e1d5dfca8627bc78369bb7c042e0
lxx897 <lxx897@163.com>
2023-11-26 00:13:39 +0800

    Site updated: 2023-11-26 00:13:37

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "EA9999",
+    "tmpSize": "16",
+    "cityColor": "E8C3E8",
+    "citySize": "16",
+    "aqiColor": "C7E0BC",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "a9e482d53f9c424db8340e45506b7aaf"
+  }
+}
